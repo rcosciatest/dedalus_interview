2e4295fb4415ceb52a98bb367f88f2cde848e5b6
Roberto Coscia <rcosciamusic@gmail.com>
2025-06-25 11:33:05 +0200

    removing comments

--- tests/imaging_tests/imaging.spec.ts
+++ tests/imaging_tests/imaging.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect, type Page } from '@playwright/test';
 
-// By default, tests run in parallel. This describe block should run sequentially
-// because the tests inside depend on each other and share a single page instance.
 test.describe.configure({ mode: 'serial' });
 
 test.describe('Imaging Feature', () => {
