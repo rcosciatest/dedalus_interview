e269227af53f0f575b032d0dbaaa1b97deabdabb
Roberto Coscia <rcosciamusic@gmail.com>
2025-06-25 12:52:43 +0200

    Correctness of Rendering of Images implementation

--- tests/imaging_tests/imaging.spec.ts
+++ tests/imaging_tests/imaging.spec.ts
@@ -1,4 +1,4 @@
-import { test, type Page } from '@playwright/test';
+import { test,expect, type Page } from '@playwright/test';
 
 test.describe.configure({ mode: 'serial' });
 
@@ -9,6 +9,11 @@ test.describe('Imaging Feature', () => {
     const context = await browser.newContext(); 
     page = await context.newPage();
     await page.goto('/');
+    
+    const welcomeDialog = page.getByTestId('welcome-popup-overlay');
+    if (await welcomeDialog.isVisible()) {
+      await page.getByTestId('welcome-popup-accept-button').click();
+    }
   });
 
   test.afterAll(async () => {
@@ -16,6 +21,30 @@ test.describe('Imaging Feature', () => {
   });
 
   test('Correctness of Rendering of Images', async () => {
+    //clicking image 2
+    
+    
+    await page.getByTestId('series-2-button').click();
+    await expect(page.getByTestId('medical-image')).toBeVisible();
+
+    await expect(page.getByTestId('patient-name')).toContainText(/john doe/i);
+    await expect(page.getByTestId('patient-id')).toContainText(/P001234567/i);
+    await expect(page.getByTestId('slice-information')).toHaveText('1 / 6');
+    //it should be a jpeg
+    const imageSrcTwo = await page.getByTestId('medical-image').getAttribute('src');
+    expect(imageSrcTwo).toMatch(/\.jpeg$/);
+    
+    await page.getByTestId('series-2-button').click();
+    
+    //click image 1
+    await page.getByTestId('series-1-button').click();
+    await expect(page.getByTestId('patient-name')).toContainText(/john doe/i);
+    await expect(page.getByTestId('patient-id')).toContainText(/P001234567/i);
+    await expect(page.getByTestId('slice-information')).toHaveText('1 / 7');
+    await page.getByTestId('series-1-button').click();
+    
+    const imageSrcOne = await page.getByTestId('medical-image').getAttribute('src');
+    expect(imageSrcOne).toMatch(/\.jpeg$/);
   });
 
   
