680f672a20c9038e8a07d01b293f2c64434196c4
Roberto Coscia <rcosciamusic@gmail.com>
2025-06-25 11:14:22 +0200

    scaffolding, removal of examples,started test layout

---
+++ tests/imaging_tests/imaging.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect, type Page } from '@playwright/test';
+
+test.describe('Imaging Feature', () => {
+  let page: Page;
+
+  test.beforeAll(async ({ browser }) => {
+    const context = await browser.newContext(); 
+    page = await context.newPage();
+    await page.goto('/');
+  });
+
+  test.afterAll(async () => {
+    await page.context().close(); 
+  });
+
+  test('Correctness of Rendering of Images', async () => {
+  });
+
+  
+  test('Navigation Between Images Using Mouse Scroll', async () => {
+    
+  });
+
+   
+  test('Switch of Series', async () => {
+    
+  });
+
+   
+  test('Verify Patient Information Overlay Displays Correct Data', async () => {
+    
+  });
+});
